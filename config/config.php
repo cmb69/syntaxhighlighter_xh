54129ec3f7371cc607022e86bbcc108cba14eb7a
cmb69 <cmbecker69@gmx.de>
2013-04-23 22:14:55 +0000

    cleaned up config.php and en.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,5 @@
 <?php
 
 $plugin_cf['syntaxhighlighter']['theme']="Default";
-$plugin_cf['syntaxhighlighter']['shd_first_line']="4";
-$plugin_cf['syntaxhighlighter']['']="";
-$plugin_cf['syntaxhighlighter']['']="";
-$plugin_cf['syntaxhighlighter']['']="";
 
 ?>
