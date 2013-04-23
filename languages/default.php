a7607f0d5be3d8fe763778423dd5e3db15f5b377
cmb69 <cmbecker69@gmx.de>
2013-04-23 23:57:39 +0000

    added default language file

---
+++ languages/default.php
@@ -0,0 +1,17 @@
+<?php
+
+$plugin_tx['syntaxhighlighter']['expand_source']="+ expand source";
+$plugin_tx['syntaxhighlighter']['help']="?";
+$plugin_tx['syntaxhighlighter']['alert']="SyntaxHighlighter\n\n";
+$plugin_tx['syntaxhighlighter']['no_brush']="Can't find brush for:";
+$plugin_tx['syntaxhighlighter']['brush_not_html_script']="Brush wasn't made for html-script option:";
+
+$plugin_tx['syntaxhighlighter']['syscheck_title']="System check";
+$plugin_tx['syntaxhighlighter']['syscheck_phpversion']="PHP version &ge; %s";
+$plugin_tx['syntaxhighlighter']['syscheck_extension']="Extension '%s' loaded";
+$plugin_tx['syntaxhighlighter']['syscheck_encoding']="Encoding 'UTF-8' configured";
+$plugin_tx['syntaxhighlighter']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['syntaxhighlighter']['syscheck_writable']="Folder '%s' writable";
+
+$plugin_tx['syntaxhighlighter']['cf_theme']="The theme of the syntax highlighting: \"Default\", \"Django\", \"Eclipse\", \"Emacs\", \"FadeToGrey\", \"MDUltra\", \"Midnight\" or \"RDark\".";
+?>
