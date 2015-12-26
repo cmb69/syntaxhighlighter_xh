ad4509b9cfa05cbd24ed132ac77407bfcb2b3978
Christoph M. Becker <cmbecker69@gmx.de>
2015-12-26 12:57:09 +0100

    adopted PHP_CodeSniffer

--- admin.php
+++ admin.php
@@ -3,11 +3,14 @@
 /**
  * Back-end of Syntaxhighlighter_XH.
  *
- * @package	Syntaxhighlighter
- * @copyright	Copyright (c) 2012-2015 Christoph M. Becker <http://3-magi.net/>
- * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @version     $Id$
- * @link	http://3-magi.net/?CMSimple_XH/Syntaxhighlighter_XH
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Syntaxhighlighter
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2015 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link      http://3-magi.net/?CMSimple_XH/Syntaxhighlighter_XH
  */
 
 
@@ -23,68 +26,82 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 /**
  * Returns the plugin version information view.
  *
- * @return string  The (X)HTML.
+ * @return string (X)HTML.
  */
 function Syntaxhighlighter_version()
 {
     global $pth;
 
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Syntaxhighlighter_XH">Syntaxhighlighter_XH</a></h1>'."\n"
-	. tag('img src="'.$pth['folder']['plugins'].'syntaxhighlighter/syntaxhighlighter.png" style="float: left; margin: 0 16px 16px 0"')
-	. '<p>Version: '.SYNTAXHIGHLIGHTER_VERSION.'</p>'."\n"
-	. '<p>Copyright &copy; 2012-2015 <a href="http://3-magi.net">Christoph M. Becker</a></p>'."\n"
-	. '<p>Powered by <a href="http://alexgorbatchev.com/SyntaxHighlighter/">Alex Gorbatchev\'s SyntaxHighlighter</a></p>'
-	. '<p style="text-align: justify">'
-	. 'This program is free software: you can redistribute it and/or modify'
-	. ' it under the terms of the GNU General Public License as published by'
-	. ' the Free Software Foundation, either version 3 of the License, or'
-	. ' (at your option) any later version.</p>'."\n"
-	. '<p style="text-align: justify">'
-	. 'This program is distributed in the hope that it will be useful,'
-	. ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	. ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	. ' GNU General Public License for more details.</p>'."\n"
-	. '<p style="text-align: justify">'
-	. 'You should have received a copy of the GNU General Public License'
-	. ' along with this program.  If not, see'
-	. ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>'."\n";
+    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Syntaxhighlighter_XH">'
+        . 'Syntaxhighlighter_XH</a></h1>' . "\n"
+        . tag(
+            'img src="' . $pth['folder']['plugins']
+            . 'syntaxhighlighter/syntaxhighlighter.png"'
+            . ' style="float: left; margin: 0 16px 16px 0"'
+        )
+        . '<p>Version: '.SYNTAXHIGHLIGHTER_VERSION.'</p>'."\n"
+        . '<p>Copyright &copy; 2012-2015 <a href="http://3-magi.net">'
+        . 'Christoph M. Becker</a></p>'."\n"
+        . '<p>Powered by <a href="http://alexgorbatchev.com/SyntaxHighlighter/">'
+        . 'Alex Gorbatchev\'s SyntaxHighlighter</a></p>'
+        . '<p style="text-align: justify">'
+        . 'This program is free software: you can redistribute it and/or modify'
+        . ' it under the terms of the GNU General Public License as published by'
+        . ' the Free Software Foundation, either version 3 of the License, or'
+        . ' (at your option) any later version.</p>' . "\n"
+        . '<p style="text-align: justify">'
+        . 'This program is distributed in the hope that it will be useful,'
+        . ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
+        . ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
+        . ' GNU General Public License for more details.</p>' . "\n"
+        . '<p style="text-align: justify">'
+        . 'You should have received a copy of the GNU General Public License'
+        . ' along with this program.  If not, see'
+        . ' <a href="http://www.gnu.org/licenses/">'
+        . 'http://www.gnu.org/licenses/</a>.</p>' . "\n";
 }
 
 
 /**
  * Returns the requirements information view.
  *
- * @return string  The (X)HTML.
+ * @return string (X)HTML.
  */
 function Syntaxhighlighter_systemCheck()
-{ // RELEASE-TODO
+{
+    // RELEASE-TODO
     global $pth, $tx, $plugin_tx;
 
     define('SYNTAXHIGHLIGHTER_PHP_VERSION', '4.0.7');
     $ptx = $plugin_tx['syntaxhighlighter'];
-    $imgdir = $pth['folder']['plugins'].'syntaxhighlighter/images/';
-    $ok = tag('img src="'.$imgdir.'ok.png" alt="ok"');
-    $warn = tag('img src="'.$imgdir.'warn.png" alt="warning"');
-    $fail = tag('img src="'.$imgdir.'fail.png" alt="failure"');
-    $o = '<h4>'.$ptx['syscheck_title'].'</h4>'
-	    .(version_compare(PHP_VERSION, SYNTAXHIGHLIGHTER_PHP_VERSION) >= 0 ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.sprintf($ptx['syscheck_phpversion'], SYNTAXHIGHLIGHTER_PHP_VERSION)
-	    .tag('br')."\n";
+    $imgdir = $pth['folder']['plugins'] . 'syntaxhighlighter/images/';
+    $ok = tag('img src="' . $imgdir . 'ok.png" alt="ok"');
+    $warn = tag('img src="' . $imgdir . 'warn.png" alt="warning"');
+    $fail = tag('img src="' . $imgdir . 'fail.png" alt="failure"');
+    $o = '<h4>' . $ptx['syscheck_title'] . '</h4>'
+        . (version_compare(PHP_VERSION, SYNTAXHIGHLIGHTER_PHP_VERSION) >= 0
+           ? $ok : $fail)
+        . '&nbsp;&nbsp;'
+        . sprintf($ptx['syscheck_phpversion'], SYNTAXHIGHLIGHTER_PHP_VERSION)
+        . tag('br') . "\n";
     foreach (array('pcre') as $ext) {
-	$o .= (extension_loaded($ext) ? $ok : $fail)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_extension'], $ext).tag('br')."\n";
+        $o .= (extension_loaded($ext) ? $ok : $fail)
+            . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext)
+            . tag('br') . "\n";
     }
     $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_magic_quotes'].tag('br').tag('br')."\n";
+        . '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes']
+        . tag('br') . tag('br') . "\n";
     $o .= (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_encoding'].tag('br')."\n";
+        . '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br') . "\n";
     $folders = array();
     foreach (array('config/', 'css/', 'languages/') as $folder) {
-	$folders[] = $pth['folder']['plugins'].'syntaxhighlighter/'.$folder;
+        $folders[] = $pth['folder']['plugins'] . 'syntaxhighlighter/' . $folder;
     }
     foreach ($folders as $folder) {
-	$o .= (is_writable($folder) ? $ok : $warn)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_writable'], $folder).tag('br')."\n";
+        $o .= (is_writable($folder) ? $ok : $warn)
+            . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder)
+            . tag('br') . "\n";
     }
     return $o;
 }
@@ -97,10 +114,11 @@ if (isset($syntaxhighlighter) && $syntaxhighlighter == 'true') {
     $o .= print_plugin_admin('off');
     switch ($admin) {
     case '':
-	$o .= Syntaxhighlighter_version() . tag('hr') . Syntaxhighlighter_systemCheck();
-	break;
+        $o .= Syntaxhighlighter_version() . tag('hr')
+            . Syntaxhighlighter_systemCheck();
+        break;
     default:
-	$o .= plugin_admin_common($action, $admin, $plugin);
+        $o .= plugin_admin_common($action, $admin, $plugin);
     }
 }
 
--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 /**
  * Front-end of Syntaxhighlighter_XH.
  *
- * @package	Syntaxhighlighter
- * @copyright	Copyright (c) 2012-2015 Christoph M. Becker <http://3-magi.net/>
- * @license	http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @version     $Id$
- * @link	http://3-magi.net/?CMSimple_XH/Syntaxhighlighter_XH
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Syntaxhighlighter
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2015 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link      http://3-magi.net/?CMSimple_XH/Syntaxhighlighter_XH
  */
 
 
@@ -29,74 +32,75 @@ define('SYNTAXHIGHLIGHTER_VERSION', '1alpha1');
 /**
  * Returns the brushes necessary to highlight the given $content.
  *
- * @param  string $cnt
+ * @param string $content A text to scan for brushes.
+ * 
  * @return array
  */
 function Syntaxhighlighter_brushes($content)
 {
     $aliases = array(
-	'applescript' => 'shBrushAppleScript',
-	'actionscript3' => 'shBrushAS3',
-	'as3' => 'shBrushAS3',
-	'bash' => 'shBrushBash',
-	'shell' => 'shBrushBash',
-	'coldfusion' => 'shBrushColdFusion',
-	'cf' => 'shBrushColdFusion',
-	'cpp' => 'shBrushCpp',
-	'c' => 'shBrushCpp',
-	'c#' => 'shBrushCSharp',
-	'c-sharp' => 'shBrushCSharp',
-	'csharp' => 'shBrushCSharp',
-	'css' => 'shBrushCSS',
-	'delphi' => 'shBrushDelphi',
-	'pascal' => 'shBrushDelphi',
-	'pas' => 'shBrushDelphi',
-	'diff' => 'shBrushDiff',
-	'patch' => 'shBrushPatch',
-	'erl' => 'shBrushErlang',
-	'erlang' => 'shBrushErlang',
-	'groovy' => 'shBrushGroovy',
-	'java' => 'shBrushJava',
-	'jfx' => 'shBrushJavaFX',
-	'javafx' => 'shBrushJavaFX',
-	'js' => 'shBrushJScript',
-	'jscript' => 'shBrushJScript',
-	'javascript' => 'shBrushJScript',
-	'perl' => 'shBrushPerl',
-	'pl' => 'shBrushPerl',
-	'php' => 'shBrushPhp',
-	'text' => 'shBrushPlain',
-	'plain' => 'shBrushPlain',
-	'py' => 'shBrushPython',
-	'python' => 'shBrushPython',
-	'ruby' => 'shBrushRuby',
-	'rails' => 'shBrushRuby',
-	'ror' => 'shBrushRuby',
-	'rb' => 'shBrushRuby',
-	'sass' => 'shBrushSass',
-	'scss' => 'shBrushSass',
-	'scala' => 'shBrushScala',
-	'sql' => 'shBrushSql',
-	'vb' => 'shBrushVb',
-	'vbnet' => 'shBrushVb',
-	'xml' => 'shBrushXml',
-	'xhtml' => 'shBrushXml',
-	'xslt' => 'shBrushXml',
-	'html' => 'shBrushXml'
+        'applescript' => 'shBrushAppleScript',
+        'actionscript3' => 'shBrushAS3',
+        'as3' => 'shBrushAS3',
+        'bash' => 'shBrushBash',
+        'shell' => 'shBrushBash',
+        'coldfusion' => 'shBrushColdFusion',
+        'cf' => 'shBrushColdFusion',
+        'cpp' => 'shBrushCpp',
+        'c' => 'shBrushCpp',
+        'c#' => 'shBrushCSharp',
+        'c-sharp' => 'shBrushCSharp',
+        'csharp' => 'shBrushCSharp',
+        'css' => 'shBrushCSS',
+        'delphi' => 'shBrushDelphi',
+        'pascal' => 'shBrushDelphi',
+        'pas' => 'shBrushDelphi',
+        'diff' => 'shBrushDiff',
+        'patch' => 'shBrushPatch',
+        'erl' => 'shBrushErlang',
+        'erlang' => 'shBrushErlang',
+        'groovy' => 'shBrushGroovy',
+        'java' => 'shBrushJava',
+        'jfx' => 'shBrushJavaFX',
+        'javafx' => 'shBrushJavaFX',
+        'js' => 'shBrushJScript',
+        'jscript' => 'shBrushJScript',
+        'javascript' => 'shBrushJScript',
+        'perl' => 'shBrushPerl',
+        'pl' => 'shBrushPerl',
+        'php' => 'shBrushPhp',
+        'text' => 'shBrushPlain',
+        'plain' => 'shBrushPlain',
+        'py' => 'shBrushPython',
+        'python' => 'shBrushPython',
+        'ruby' => 'shBrushRuby',
+        'rails' => 'shBrushRuby',
+        'ror' => 'shBrushRuby',
+        'rb' => 'shBrushRuby',
+        'sass' => 'shBrushSass',
+        'scss' => 'shBrushSass',
+        'scala' => 'shBrushScala',
+        'sql' => 'shBrushSql',
+        'vb' => 'shBrushVb',
+        'vbnet' => 'shBrushVb',
+        'xml' => 'shBrushXml',
+        'xhtml' => 'shBrushXml',
+        'xslt' => 'shBrushXml',
+        'html' => 'shBrushXml'
     );
 
     preg_match_all('/<pre.*?class=["\'](.*?)["\'].*?>/isu', $content, $matches);
     $brushes = array();
     foreach ($matches[1] as $class) {
-	$opts = explode(';', $class);
-	foreach ($opts as $opt) {
-	    $opt = array_map('trim', explode(':', $opt));
-	    if ($opt[0] == 'brush'
-		&& !array_key_exists($aliases[$opt[1]], $brushes)
-	    ) {
-		$brushes[] = $aliases[$opt[1]];
-	    }
-	}
+        $opts = explode(';', $class);
+        foreach ($opts as $opt) {
+            $opt = array_map('trim', explode(':', $opt));
+            if ($opt[0] == 'brush'
+                && !array_key_exists($aliases[$opt[1]], $brushes)
+            ) {
+                $brushes[] = $aliases[$opt[1]];
+            }
+        }
     }
     return $brushes;
 }
@@ -105,20 +109,22 @@ function Syntaxhighlighter_brushes($content)
 /**
  * Writes the necessary JS and CSS to the head element.
  *
+ * @param string $content A text to scan for brushes.
+ * 
+ * @return void
+ * 
  * @global array  The paths of system files and folders.
- * @global string  Elements to be inserted in the head element.
+ * @global string Elements to be inserted in the head element.
  * @global array  The configuration of the plugins.
  * @global array  The localization of the plugins.
- * @param  string $content
- * @return void
  */
-function Syntaxhighlighter($content)
+function syntaxhighlighter($content)
 {
     global $pth, $hjs, $plugin_cf, $plugin_tx;
 
     $brushes = syntaxhighlighter_brushes($content);
     if (empty($brushes)) {
-	return;
+        return;
     }
 
     $pcf = $plugin_cf['syntaxhighlighter'];
@@ -126,39 +132,33 @@ function Syntaxhighlighter($content)
     $dir = $pth['folder']['plugins'].'syntaxhighlighter/';
 
     foreach (array('shCore', 'shBrushXml') as $f) {
-	$fn = $dir . 'lib/scripts/' . $f . '.js';
-	$hjs .= '<script type="text/javascript" src="' . $fn . '"></script>'
-	    . "\n";
+        $fn = $dir . 'lib/scripts/' . $f . '.js';
+        $hjs .= '<script type="text/javascript" src="' . $fn . '"></script>'
+            . "\n";
     }
     foreach ($brushes as $brush) {
-	$fn = $dir . 'lib/scripts/' . $brush . '.js';
-	$hjs .= '<script type="text/javascript" src="' . $fn. '"></script>'
-	    . "\n";
+        $fn = $dir . 'lib/scripts/' . $brush . '.js';
+        $hjs .= '<script type="text/javascript" src="' . $fn. '"></script>'
+            . "\n";
     }
     foreach (array('shCore', 'shTheme') as $f) {
-	$fn = $dir . '/lib/styles/' . $f . $pcf['theme'] . '.css';
-	$hjs .= tag('link rel="stylesheet" href="' . $fn . '" type="text/css"')
-	    . "\n";
+        $fn = $dir . '/lib/styles/' . $f . $pcf['theme'] . '.css';
+        $hjs .= tag('link rel="stylesheet" href="' . $fn . '" type="text/css"')
+            . "\n";
     }
 
     $strs = array();
     $keys = array('expand_source', 'help', 'alert', 'no_brush',
-	'brush_not_html_script');
+        'brush_not_html_script');
     foreach ($keys as $key) {
-	$jskey = substr($key, 0, 1)
-	    . substr(implode('', array_map('ucfirst', explode('_', $key))), 1);
-	$strs[] = $jskey . " = '"
-	    . addcslashes($ptx[$key], "\0..\37\"\'\\") . "';";
+        $jskey = substr($key, 0, 1)
+            . substr(implode('', array_map('ucfirst', explode('_', $key))), 1);
+        $strs[] = $jskey . " = '"
+            . addcslashes($ptx[$key], "\0..\37\"\'\\") . "';";
     }
     $strs = implode("\n        ", $strs);
 
     $defs = array();
-//    foreach ($pcf as $opt => $val) {
-//	list($cat, $key) = explode('_', $opt, 2);
-//	if ($cat == 'shd') {
-//	    $defs[] = "defaults['" . str_replace('_', '-', $key) . "'] = 3;";
-//	}
-//    }
     $defs = implode("\n    ", $defs);
 
     $hjs .= <<<SCRIPT
@@ -166,7 +166,7 @@ function Syntaxhighlighter($content)
 /* <![CDATA[ */
 with (SyntaxHighlighter) {
     with (config.strings) {
-	$strs
+        $strs
     }
     $defs
     all()
