4e023dc6f6dce859d834b1766af2af05fc46eb15
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-09 13:20:03 +0100

    Indent XML and PHTML files with two spaces

--- views/info.php
+++ views/info.php
@@ -11,8 +11,8 @@ namespace Syntaxhighlighter;
 
 <h1>Syntaxhighlighter <?=$this->escape($version)?></h1>
 <div class="syntaxhighlighter_syscheck">
-    <h2><?=$this->text('syscheck_title')?></h2>
+  <h2><?=$this->text('syscheck_title')?></h2>
 <?php foreach ($checks as $check):?>
-    <p class="xh_<?=$this->escape($check->state)?>"><?=$this->text('syscheck_message', $check->label, $check->stateLabel)?></p>
+  <p class="xh_<?=$this->escape($check->state)?>"><?=$this->text('syscheck_message', $check->label, $check->stateLabel)?></p>
 <?php endforeach?>
 </div>
